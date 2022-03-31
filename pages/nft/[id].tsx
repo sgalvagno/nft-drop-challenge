bffcf053583c1110560f9bba32b8ddb6bfc38491
Galvagno Sebastien <gitlab@waveye.fr>
2022-03-31 16:22:15 +0200

    add padding p-2 on the image

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -5,7 +5,7 @@ function NFTDropPage() {
        {/* Left */} 
        <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
             <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
-                <div className='bg-gradient-to-br from-yellow-400 to-purple-600'>
+                <div className='bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
                     <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72' src="https://links.papareact.com/8sg" alt=""/>
                 </div>
                 <div>
