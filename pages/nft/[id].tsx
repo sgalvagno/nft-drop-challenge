75ff0659d4bbef19b1c00ecc706b344246cef5f0
Galvagno Sebastien <gitlab@waveye.fr>
2022-03-31 17:56:22 +0200

    fix the grid size to 4 cols for left part

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NFTDropPage() {
   return <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
        {/* Left */} 
-       <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
+       <div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
             <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
                 <div className='rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
                     <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72' src="https://links.papareact.com/8sg" alt=""/>
